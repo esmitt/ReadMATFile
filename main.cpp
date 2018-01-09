7b53147dd462b454d04e3508c0957b10e1aa5032
esmitt ramirez <esmitt.ramirez@cvc.uab.es>
2018-01-09 15:12:37 +0100

    deleting useless stuff

--- main.cpp
+++ main.cpp
@@ -12,87 +12,14 @@ using namespace std;
 #pragma comment(lib, "lib/libmx.lib")
 #pragma comment(lib, "lib/libmat.lib")
 
-//class CDataTree
-//{
-//public:
-//  int m_iMatLabIndex;
-//  int m_inPoints;
-//  CDataTree() 
-//  {
-//    m_iMatLabIndex = -1;
-//    m_inPoints = 0;
-//  }
-//};
-//
-//class CMatTreeNode
-//{
-//public:
-//  CDataTree m_data;
-//  int m_iIndex;
-//  int m_iParent;
-//  int m_iChildL;
-//  int m_iChildR;
-//
-//  CMatTreeNode()
-//  {
-//    m_iIndex = -1;
-//    m_iParent = -1;
-//    m_iChildL = -1;
-//    m_iChildR = -1;
-//  }
-//
-//  void printNode()
-//  {
-//    cout << "index: " << m_iIndex << endl;
-//    cout << "Matlab-index: " << m_data.m_iMatLabIndex << endl;
-//    cout << "points: " << m_data.m_inPoints << endl;
-//    cout << "parent " << m_iParent << endl;
-//    cout << "LChild-index " << m_iChildL << endl;
-//    cout << "RChild-index " << m_iChildR << endl;
-//  }
-//};
-//
-//enum class NODE_TYPE {ROOT, LEFT, RIGHT, NONE};
-typedef struct point3d
+struct point3d
 {
   float x, y, z;
   point3d() {}
   point3d(float p[3]) { x = p[0], y = p[1], z = p[2]; }
 };
 
-/// Determines the equivalent subscript values corresponding to the absolute index dimension of a multidimensional array.
-/// @param siz size of the N-dimensional matrix
-/// @param N the dimensions of the matrix
-/// @param idx index in linear format
-/// @param sub the output - subscript values written into an N - dimensional integer array (created by caller)
-void ind2sub(const size_t *siz, int N, int idx, int *sub)
-{
-  // Calculate the cumulative product of the size vector
-  std::vector<int> cumProd(N);
-  int nextProd = 1;
-  for (int k = 0; k < N; k++)
-  {
-    cumProd[k] = nextProd;
-    nextProd *= siz[k];
-  }
-
-  // Validate range of linear index
-  int maxInd = cumProd[N - 1] * siz[N - 1] - 1;	//assuming that fit into a int data type
-  if ((idx < 0) || (idx > maxInd))
-  {
-    std::cout << "bad linear index maxsum::ind2sub" << std::endl;	//some kind of message
-    return;
-  }
-
-  //the computation
-  int remainder = idx;
-  for (int k = N - 1; k >= 0; k--)
-  {
-    sub[k] = remainder / cumProd[k];
-    remainder = remainder % cumProd[k];
-  }
-}
-
+/// A node element, which contains who is its father, and the list of point to reach it
 class CTreeNode
 {
 public:
@@ -101,37 +28,56 @@ public:
   CTreeNode() : m_iParent { -1 } {  }
 };
 
-class Mat2CPP 
+
+class CArrayTree
 {
 private:
-  std::vector<std::vector<int>> m_matrixE;
-  MATFile* m_pFileMat;
-  mxArray *m_pArray;
-
-//  std::vector<CMatTreeNode> vecTree;
   std::vector<CTreeNode> m_tree;
-public:
-  Mat2CPP()  
+  /// Determines the equivalent subscript values corresponding to the absolute index dimension of a multidimensional array.
+  /// @param siz size of the N-dimensional matrix
+  /// @param N the dimensions of the matrix
+  /// @param idx index in linear format
+  /// @param sub the output - subscript values written into an N - dimensional integer array (created by caller)
+  void ind2sub(const size_t *siz, int N, int idx, int *sub)
   {
-    m_pFileMat = nullptr;
-    m_pArray = nullptr;
-  }
-  
-  //void printTree() 
-  //{
-  //  for_each(vecTree.begin(), vecTree.end(), [](CMatTreeNode& node) {
-  //    if(node.m_iIndex > 0)
-  //      node.printNode();
-  //  });
-  //}
+    // Calculate the cumulative product of the size vector
+    std::vector<int> cumProd(N);
+    int nextProd = 1;
+    for (int k = 0; k < N; k++)
+    {
+      cumProd[k] = nextProd;
+      nextProd *= siz[k];
+    }
 
+    // Validate range of linear index
+    int maxInd = cumProd[N - 1] * siz[N - 1] - 1;	//assuming that fit into a int data type
+    if ((idx < 0) || (idx > maxInd))
+    {
+      std::cout << "bad linear index maxsum::ind2sub" << std::endl;	//some kind of message
+      return;
+    }
+
+    //the computation
+    int remainder = idx;
+    for (int k = N - 1; k >= 0; k--)
+    {
+      sub[k] = remainder / cumProd[k];
+      remainder = remainder % cumProd[k];
+    }
+  }
+public:
+  CArrayTree() {  }
+  ~CArrayTree() {  }
+
+  // Load a .mat file into the tree structure
+  // @param name of the filename to open
   bool loadMATFile(string strFileName)
   {
-    m_pFileMat = matOpen(strFileName.c_str(), "r");
+    MATFile* m_pFileMat = matOpen(strFileName.c_str(), "r");
     if (m_pFileMat == nullptr) return false;
 
     //load variable G
-    m_pArray = matGetVariable(m_pFileMat, "GFinalDir");
+    mxArray* m_pArray = matGetVariable(m_pFileMat, "GFinalDir");
     //get size volume
     //const mwSize *dimsE = mxGetDimensions(pArrToE);
     mxArray *pArraySkeleton = matGetVariable(m_pFileMat, "skel");
@@ -145,14 +91,14 @@ public:
     double* pValue = mxGetPr(pArrToE);
 
     //get the field we
-    mxArray *pArrToWE = mxGetField(m_pArray, 0, "we");
+    mxArray *pArrToWE = mxGetField(m_pArray, 0, "we");  //this is a cell
 
     //create the tree
-    //vecTree.resize(iWidth);
     m_tree.resize(iWidth + 1);
 
     //create matrix E entirely 
-    //matlab stores the data column-order
+    //matlab stores the data column-order - IMPORTANT! -
+    std::vector<std::vector<int>> m_matrixE;
     m_matrixE.resize(iHeight);
     for (mwIndex y = 0; y < iHeight; y++)
       m_matrixE[y].resize(iWidth);
@@ -165,12 +111,13 @@ public:
         m_matrixE[x][y] = pValue[(y*iWidth) + x];
         if (m_matrixE[x][y] >= 1)
         {
-          //m_tree[(y+1)].m_inPoints = m_matrixE[x][y];
-          m_tree[(y+1)].m_iParent = (x + 1);
+          m_tree[(y+1)].m_iParent = (x + 1);  //copy the value
+
           //now, extract points
           mxArray *pCellContent = mxGetCell(pArrToWE, [](int x, int y, int width) -> size_t { //return the linear index of x, y
             return (width*y) + x;
           }(x, y, iWidth));
+          //now, we have the number of elements of GFinalDir.we
           const mwSize pNumElements = mxGetDimensions(pCellContent)[1];  //always is 1 x k, where k is the size. then, the position 0 is always 1
           double* pValueWE = mxGetPr(pCellContent);
           for (int k = 0; k < pNumElements; k++) 
@@ -186,10 +133,11 @@ public:
         }
       }
     }
-    //printTree();
     return true;
   }
 
+  // From root (node 1), this prints all points to reach until iNode
+  // @param iNode destination node
   void printPointsPath(int iNode) 
   {
     int iTempNode = iNode;
@@ -202,8 +150,8 @@ public:
     path.push_back(1);
     std::reverse(path.begin(), path.end());
 
-    //for_each(path.begin(), path.end(), [=](int index) 
-    for_each(path.begin(), path.begin() + 2, [=](int index)
+    for_each(path.begin(), path.end(), [=](int index) 
+    //for_each(path.begin(), path.begin() + 5, [=](int index)
     {
       for_each(m_tree[index].m_vecPoints.begin(), m_tree[index].m_vecPoints.end(), [](point3d p) {
         cout << p.x << " " << p.y << " " << p.z << endl;
@@ -211,6 +159,7 @@ public:
     });
   }
 
+  // From root (node 1), prints the sequence in format a --> b --> ... --> iNode
   void printPath(int iNode)
   {
     int iTempNode = iNode;
@@ -221,118 +170,26 @@ public:
       iTempNode = m_tree[iTempNode].m_iParent;
     }
     path.push_back(1);
-    std::reverse(path.begin(), path.end());
+    std::reverse(path.begin(), path.end()); //are stored in backward order, then reverse it
+
     for_each(path.begin(), path.end(), [](int index) {
       cout << index << " --> ";
     });
-  }
-
-  void printPath(int iStartNode, int iEndNode)
-  {
-    if (iEndNode != iStartNode) 
-    {
-      printPath(m_tree[iStartNode].m_iParent, iEndNode);
-      cout << iStartNode << " --> ";
-    }
-  }
-
-  //void compactTree() 
-  //{
-  //  for (int iIndex = 1; iIndex < vecTree.size(); iIndex++) 
-  //  {
-  //    if(vecTree[iIndex].m_iIndex > -1) //if exists
-  //    {
-  //      int iRoot = vecTree[iIndex].m_iIndex;
-  //      //check if the root is child on another subtree
-  //      for (int child = 1; child < vecTree.size(); child++) 
-  //      {
-  //        if (child != iIndex && vecTree[child].m_iIndex > -1) //if exists
-  //        {
-  //          //check if exist on left child
-  //          if (iRoot == vecTree[child].m_iChildL) 
-  //          {
-  //            vecTree[iIndex].m_iParent = child;
-  //            break;
-  //          }
-  //          if (iRoot == vecTree[child].m_iChildR)
-  //          {
-  //            vecTree[iIndex].m_iParent = child;
-  //            break;
-  //          }
-  //        }
-  //      }
-  //    }
-  //  }
-  //}
-
-  void print() 
-  {
-    for (mwIndex x = 0; x < m_matrixE.size(); x++)
-      for (mwIndex y = 0; y < m_matrixE.size(); y++)
-        if(m_matrixE[x][y] >= 1)
-          cout << "(" << (x + 1) << "," << (y + 1) << ") = " << m_matrixE[x][y] << endl;
-  }
-
-  ~Mat2CPP() 
-  {
-    if(m_pFileMat != nullptr)
-      matClose(m_pFileMat);
-    if(m_pArray != nullptr)
-      mxDestroyArray(m_pArray);
   }
 };
 
 int main() 
 {
   string strFilename = "C:\\Users\\eramirez\\Desktop\\CARS\\Cases\\28_INSP_CPAP_Graph_skel_vol.mat";
-  Mat2CPP app;
+  CArrayTree app;
   if (app.loadMATFile(strFilename)) 
   {
-    //app.print();
     //app.printPath(2);
-    app.printPointsPath(2);
+    app.printPointsPath(40);
   }
   else 
   {
     cout << "some error reading" << endl;
   }
-  //MATFile* m_pFileMat = matOpen(strFilename.c_str(), "r");
-  //if (m_pFileMat == nullptr)
-  //  return EXIT_FAILURE;
-  //
-  //mxArray *pArr = matGetVariable(m_pFileMat, "G");
-  //mwSize num = mxGetNumberOfFields(pArr);	//number of fields
-
-  //mxArray *pArrToE = mxGetField(pArr, 0, "e");	//get the field 
-  ////mwSize iDimE = mxGetNumberOfElements(pArrToE);
-  //const mwSize *dims = mxGetDimensions(pArrToE);
-  //double* pValue = mxGetPr(pArrToE);
-  //for (mwIndex j = 0; j < dims[1]; j++)
-  //  for (mwIndex i = 0; i < dims[0]; i++) 
-  //  {
-  //      //cout << (i + 1) << " " << (j + 1) << endl;
-  //    if (pValue[(j*dims[0]) + i] > 0) 
-  //    {
-  //      cout << "(" << i << "," << j << ") = " << pValue[(j*dims[0]) + i] << endl;
-  //    }
-  //  }
-  //double *pr = mxGetPr(pArrToE);
-
-  //mxArray *pArrToWE = mxGetField(pArr, 0, "we");
-  //mxArray *cellElement;
-  //const mwSize *dims = mxGetDimensions(pArrToWE);
-  //
-  //for (mwIndex jcell = 0; jcell < dims[0]; jcell++)
-  //{
-  //  cellElement = mxGetCell(pArrToWE, jcell);
-  //  if (cellElement != nullptr)
-  //  {
-  //    double* p = mxGetPr(cellElement);
-  //    mexPrintf("The content at is %g\n", *p);
-  //  }
-  //}
-
-//  mxDestroyArray(pArr);
-//  matClose(m_pFileMat);
   return EXIT_SUCCESS;
 }
